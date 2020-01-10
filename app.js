7a1eff9a494ddb37d3b57efe8c5a7a7e46fd02f5
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2020-01-10 11:05:59 -0600

    works but want to do a parallel for loop

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
-const Nightmare = require('nightmare');
 const log4js = require('log4js');
 var logger = log4js.getLogger();
 logger.level = 'debug';
 var login = require('./login.js');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+// @ts-ignore
+puppeteer.use(StealthPlugin());
 
 var nightmare = null;
 logger.info('start');
 
 main();
-function main() {
-	logger.info(new Date(), 'Logging into YouTube Studio to add users to private videos');
-	nightmare = new Nightmare({ show: true });
-	// var videos = [ 'videourl' ];
-	nightmare
-		.goto(login.youtubeUrl)
-		.insert('#Email', login.email)
-		.click('.rc-button.rc-button-submit')
-		.wait('#Passwd')
-		.insert('#Passwd', login.pass)
-		.click('.rc-button.rc-button-submit')
-		.wait('.page-title-container')
-		.goto(login.youtubeVideo1)
-		.wait('.yt-uix-form-input-textarea .metadata-share-contacts')
-		.insert('.yt-uix-form-input-textarea .metadata-share-contacts', 'email@gmail.com')
-		.then(() => {
-			logger.info('Finished sharing private videos');
-			// nightmare.end();
-			// nightmare = null;
-		})
-		.catch((error) => {
-			const errorTitle = 'Failed to share private videos';
-			logger.error(errorTitle, error);
-			// nightmare.end();
-			// nightmare = null;
-		})
-		.then((_) => {
-			logger.debug('Ending nightmare');
-			nightmare.end();
-			nightmare.proc.disconnect();
-			nightmare.proc.kill();
-			nightmare.ended = true;
-			nightmare = null;
-		});
+async function main() {
+	for (let i = 0; i < login.youtubeVideos.length; i++) {
+		var promise1 = addEmailsToVideo(login.youtubeVideos[i]);
+
+		await Promise.all([ promise1 ]);
+	}
+}
+
+function addEmailsToVideo(videoUrl) {
+	return new Promise(async (resolve, reject) => {
+		logger.info('video url', videoUrl);
+
+		logger.info(new Date(), 'Logging into YouTube Studio to add users to private videos');
+		// var videos = [ 'videourl' ];
+		// @ts-ignore
+		const browser = await puppeteer.launch({ headless: false }); // default is true
+		const page = await browser.newPage();
+		await page.goto(login.youtubeUrl, { waitUntil: 'networkidle2' });
+		await page.type('input[type="email"]', login.email);
+		await page.type('body', '\u000d');
+		await page.waitForNavigation();
+		// await page.waitFor('input[type="password"]');
+		await page.waitFor(5000);
+		await page.type('input[type="password"]', login.pass);
+		await page.type('body', '\u000d');
+		await page.waitForNavigation();
+		await page.goto(videoUrl, { waitUntil: 'networkidle2' });
+		// await page.waitForNavigation();
+		await page.waitFor('.yt-uix-form-input-textarea.metadata-share-contacts');
+		await page.type('.yt-uix-form-input-textarea.metadata-share-contacts', login.inputEmail);
+		await page.click('.yt-uix-button.yt-uix-button-size-default.yt-uix-button-primary.sharing-dialog-button.sharing-dialog-ok');
+		await page.waitFor('.yt-dialog-waiting-content');
+		await browser.close();
+		resolve();
+	});
 }
