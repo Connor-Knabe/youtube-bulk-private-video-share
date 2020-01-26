558b4b778c996e9124e76c820d22ae52165676dd
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2020-01-26 12:33:47 -0600

    closing browser if error happens

--- loginExample.js
+++ loginExample.js
@@ -1,9 +1,2 @@
 module.exports.email = 'blah@blah.com';
 module.exports.pass = 'blah';
-module.exports.inputEmail = 'example@gmail.com';
-module.exports.youtubeUrl = 'https://studio.youtube.com/channel/lkajsdf0aj02fe';
-module.exports.youtubeVideos = [
-	'https://www.youtube.com/edit?video_id=asdfasdf23&nps=1',
-	'https://www.youtube.com/edit?video_id=asdfasdf23rf&nps=1',
-	'https://www.youtube.com/edit?video_id=asdfasdfh9&nps=1'
-];
