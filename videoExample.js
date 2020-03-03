ffc1c084bb32a98ce785d2a344b658d25fd4088e
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2020-03-03 13:17:56 -0600

    fixing issue that it would break when adding emails

--- videoExample.js
+++ videoExample.js
@@ -1,3 +1,13 @@
 module.exports.inputEmails = 'example@example.com, example@example.com';
 module.exports.youtubeUrl = 'https://studio.youtube.com/channel/j20ejf02jef0';
-module.exports.youtubeVideoIds = [ 'asdfasdf1', 'asdfj20ff', 'asdfj02f' ];
+
+module.exports.youtubeVideos = [
+	{
+		videoId: 'asdfasdf1',
+		retryCount: 0
+	},
+	{
+		videoId: 'asdfasdf2',
+		retryCount: 0
+	}
+];
