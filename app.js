1f878120a73790aee079aefb01774dd89f6ffdc4
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2020-03-04 12:52:07 -0600

    working on leaving browser open to add all video permissions

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const videoFile = require('./video.js');
 const options = require('./options.js');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+var page = null;
+var browser = null;
 
 // @ts-ignore
 puppeteer.use(StealthPlugin());
@@ -18,6 +20,8 @@ logger.info('start');
 main(videoFile.youtubeVideos);
 
 async function main(vids) {
+	await setupBrowser();
+
 	for (let vidNum = 0; vidNum < vids.length; vidNum += options.parallelBrowserCount) {
 		var browsers = [];
 		for (let browserNum = 0; browserNum < options.parallelBrowserCount; browserNum++) {
@@ -38,6 +42,8 @@ async function main(vids) {
 
 	if (vids && vids != undefined && vids.length > 0) {
 		checkForRetry(vids);
+	} else {
+		browser.close();
 	}
 }
 
@@ -63,36 +69,44 @@ function checkForRetry(vids) {
 
 		if (vidsNeedToRetry && vidsNeedToRetry[0] != undefined && vidsNeedToRetry.length > 0) {
 			console.log('need to retry', vidsNeedToRetry);
+			browser.close();
 			main(vidsNeedToRetry);
 		}
 	}
 }
+
+async function setupBrowser() {
+	return new Promise(async (resolve, reject) => {
+		browser = await puppeteer.launch({ headless: options.disableBrowserWindow });
+
+		page = await browser.newPage();
+		await page.goto(videoFile.youtubeUrl, { waitUntil: 'networkidle2' });
+		await page.type('input[type="email"]', login.email);
+		await page.type('body', '\u000d');
+		await page.waitForNavigation();
+		await page.waitFor(2000);
+		await page.type('input[type="password"]', login.pass);
+		await page.type('body', '\u000d');
+		await page.waitForNavigation();
+		resolve();
+	});
+}
+
 function addEmailsToVideo(vid) {
 	return new Promise(async (resolve, reject) => {
-		const browser = await puppeteer.launch({ headless: options.disableBrowserWindow });
 		var videoId = vid.videoId;
 		try {
 			// @ts-ignore
-			const page = await browser.newPage();
-			await page.goto(videoFile.youtubeUrl, { waitUntil: 'networkidle2' });
-			await page.type('input[type="email"]', login.email);
-			await page.type('body', '\u000d');
-			await page.waitForNavigation();
-			await page.waitFor(2000);
-			await page.type('input[type="password"]', login.pass);
-			await page.type('body', '\u000d');
-			await page.waitForNavigation();
+
 			await page.goto(`https://www.youtube.com/edit?video_id=${videoId}&nps=1`, { waitUntil: 'networkidle2' });
 
 			if (options.removeOnAdd) {
 				await page.click('.sharing-dialog-remove-all-container.control-small-text');
+				await page.waitFor(1000);
+				await page.click('.yt-uix-button.yt-uix-button-size-default.yt-uix-button-primary.sharing-dialog-button.sharing-dialog-ok');
+				await page.waitFor(5000);
+				await page.goto(`https://www.youtube.com/edit?video_id=${videoId}&nps=1`, { waitUntil: 'networkidle2' });
 			}
-
-			await page.click('.yt-uix-button.yt-uix-button-size-default.yt-uix-button-primary.sharing-dialog-button.sharing-dialog-ok');
-
-			await page.waitFor(5000);
-
-			await page.goto(`https://www.youtube.com/edit?video_id=${videoId}&nps=1`, { waitUntil: 'networkidle2' });
 
 			await page.waitFor('.yt-uix-form-input-textarea.metadata-share-contacts');
 			await page.type('.yt-uix-form-input-textarea.metadata-share-contacts', videoFile.inputEmails);
@@ -106,11 +120,11 @@ function addEmailsToVideo(vid) {
 
 			await page.click('.yt-uix-button.yt-uix-button-size-default.yt-uix-button-primary.sharing-dialog-button.sharing-dialog-ok');
 			await page.waitFor(5000);
-			await browser.close();
+			// await browser.close();
 			resolve(videoId);
 		} catch (exception) {
 			vid.retryCount++;
-			await browser.close();
+			// await browser.close();
 			var result = {
 				error: exception,
 				videoId: videoId
