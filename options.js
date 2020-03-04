f1dea59fe6c83d4706a0db0fcd869093300f2a8a
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2020-03-04 12:36:01 -0600

    now correctly removes and readds emails to the video

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 module.exports.parallelBrowserCount = 1;
 module.exports.disableEmailNotification = true;
-module.exports.disableBrowserWindow = true;
+module.exports.disableBrowserWindow = false;
 module.exports.removeOnAdd = true;
 module.exports.maxRetryCount = 3;
